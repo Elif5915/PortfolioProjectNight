2a62ff6d89d089376c645fa014f2f581a6809f70
Elif Yıldırım <elifyildirim3@outlook.com>
2024-09-19 21:45:54 +0300

    What was done in lesson 4

---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using PortfolioProjectNight.Models;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult createSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult createSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public ActionResult deleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+              
+        }
+
+        [HttpGet]
+        public ActionResult updateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
+    }
+}
