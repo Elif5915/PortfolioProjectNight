4f084bf3e24531fa18dbfd48d3950c0e16ac0167
Elif Yıldırım <elifyildirim3@outlook.com>
2024-09-26 21:52:11 +0300

    What I did in 5 lessons

---
+++ PortfolioProjectNight/Controllers/ExperienceController.cs
@@ -0,0 +1,59 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult ExperienceList()
+        {
+            var values = context.Experience.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult createExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult createExperience(Experience experience)
+        {
+            context.Experience.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public ActionResult deleteExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            context.Experience.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public ActionResult updateExperience(int id)
+        {
+            var value = context.Experience.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult updateExperience(Experience experience)
+        {
+            var value = context.Experience.Find(experience.ExperienceId);
+            value.SubTitle = experience.SubTitle;
+            value.Title = experience.Title;
+            value.Description = experience.Description;
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+    
+}
--- PortfolioProjectNight/Controllers/MessageController.cs
+++ PortfolioProjectNight/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PortfolioProjectNight.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace PortfolioProjectNight.Controllers
 {
     public class MessageController : Controller
     {
-        // GET: Message
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         public ActionResult Inbox()
         {
-            return View();
+            var values = context.Contact.ToList();
+            return View(values);
+        }
+
+        public ActionResult changeMessageStatusToTrue(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult changeMessageStatusToFalse(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
         }
     }
 }
