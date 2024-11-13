9248b16cd29313afe75d8c9612feeab12e955639
Elif Yıldırım <elifyildirim3@outlook.com>
2024-11-13 23:58:42 +0300

    ...

---
+++ PortfolioProjectNight/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class DashboardController : Controller
+    {
+        DbMyPortfolioNightEntities entities = new DbMyPortfolioNightEntities();
+        public ActionResult Index()
+        {
+            ViewBag.eduCount = entities.Education.Count();
+            ViewBag.skill = entities.Skill.Count();
+            ViewBag.service = entities.Service.Count();
+            return View();
+        }
+    }
+}
