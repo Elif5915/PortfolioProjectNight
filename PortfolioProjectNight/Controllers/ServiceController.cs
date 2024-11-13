8007a578c558a11d1c938d28a801f4ecf1e516d3
Elif Yıldırım <elifyildirim3@outlook.com>
2024-11-13 22:49:56 +0300

    continue homework project

--- PortfolioProjectNight/Controllers/ServiceController.cs
+++ PortfolioProjectNight/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PortfolioProjectNight.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,51 @@ namespace PortfolioProjectNight.Controllers
 {
     public class ServiceController : Controller
     {
-        // GET: Service
+        DbMyPortfolioNightEntities entities = new DbMyPortfolioNightEntities();
         public ActionResult ServiceList()
         {
-            return View();
+            var values = entities.Service.ToList();
+            return View(values);
         }
+        public ActionResult CreateService()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult CreateService(Service service)
+		{
+            entities.Service.Add(service);
+            entities.SaveChanges();
+
+          return RedirectToAction("ServiceList");
+		}
+
+        public ActionResult UpdateService(int id)
+		{
+            var data = entities.Service.Find(id);
+            return View(data);
+		}
+
+        [HttpPost]
+        public ActionResult UpdateService(Service service)
+		{
+            var data = entities.Service.Find(service.ServiceId);
+            data.Title = service.Title;
+            data.Description = service.Description;
+
+            entities.SaveChanges();
+            return RedirectToAction("ServiceList");
+		}
+
+        public ActionResult deleteService(int id)
+		{
+           var data = entities.Service.Find(id);
+            entities.Service.Remove(data);
+            entities.SaveChanges();
+
+            return RedirectToAction("ServiceList");
+
+		}
     }
 }
